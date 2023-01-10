512446b25d7be13bdc97b335a8e6abd3b541a27d
tieckby <tiemokosogodogo97@gmail.com>
2023-01-10 08:38:40 +0000

    Laravel Test Web Dev

---
+++ resources/views/about.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('title', 'About')
+
+@section('content')
+    <h1>This's About Page</h1>
+    <div class="main">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur provident possimus accusamus, error, aut assumenda asperiores esse reprehenderit minima dolores ipsa iure maxime vel quod laboriosam obcaecati quae nesciunt alias?
+    </div>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('title', 'Home')
+
+@section('content')
+    <h1>This's Home</h1>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur provident possimus accusamus, error, aut assumenda asperiores esse reprehenderit minima dolores ipsa iure maxime vel quod laboriosam obcaecati quae nesciunt alias?
+    </div>
+@endsection
